5655ef3aaccfffb48597d527baee2734822e5d9d
Ash-ku-thakur <bhavesh10683@gmail.com>
2024-06-20 11:37:31 +0530

    Initialize the Project

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import Feed from "./Feed";
+import Leftsidebar from "./Leftsidebar";
+import RightSidebar from "./RightSidebar";
+
+const Home = () => {
+  return (
+    <div>
+      <Leftsidebar />
+      <Feed />
+      <RightSidebar />
+    </div>
+  );
+};
+
+export default Home;
